d5d488e07d16fe1f5400c398ab262333496f8777
miku86 <miku86coding@gmail.com>
2020-05-20 13:43:53 +0200

    Add store

--- src/state/store.ts
+++ src/state/store.ts
@@ -1,10 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
-import friendsReducer from "./friendsSlice";
 
 const store = configureStore({
-  reducer: {
-    friends: friendsReducer,
-  }
+  reducer: {}
 });
 
 export type AppDispatch = typeof store.dispatch;
