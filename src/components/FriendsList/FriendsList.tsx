b85976025ce039959d3245f7bd971d78a53f93e2
miku86 <miku86coding@gmail.com>
2020-05-20 19:36:23 +0200

    Use list components

--- src/components/FriendsList/FriendsList.tsx
+++ src/components/FriendsList/FriendsList.tsx
@@ -1,24 +1,51 @@
+import IconButton from "@material-ui/core/IconButton";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemSecondaryAction from "@material-ui/core/ListItemSecondaryAction";
+import ListItemText from "@material-ui/core/ListItemText";
+import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
+import { Delete } from '@material-ui/icons';
 import React, { useEffect } from "react";
 import { connect } from "react-redux";
 import { loadFriends } from "../../state/friendsSlice";
+import { convertTimestampToHumanTime } from "../../utils/date";
 import { AppState, Friend } from "../../utils/types";
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      width: "100%",
+      maxWidth: 360,
+      backgroundColor: theme.palette.background.paper,
+    },
+  })
+);
 
 interface Props {
   loadFriends?: () => void;
   friends?: Friend[];
 }
 
-export const FriendsList = ({friends, loadFriends}: Props) => {
+export const FriendsList = ({ friends, loadFriends }: Props) => {
+  const classes = useStyles();
+
   useEffect(() => {
     loadFriends!();
   }, [loadFriends]);
 
   return friends ? (
-    <ul>
-      {friends.map(({ friendId, friendName }) => (
-        <li key={friendId}>{friendName}</li>
+    <List className={classes.root}>
+      {friends.map(({ friendId, friendName, lastTimeContacted }) => (
+        <ListItem key={friendId} role={undefined} dense button>
+          <ListItemText primary={friendName} secondary={convertTimestampToHumanTime(lastTimeContacted)}/>
+          <ListItemSecondaryAction>
+            <IconButton edge="end" aria-label="comments">
+              <Delete />
+            </IconButton>
+          </ListItemSecondaryAction>
+        </ListItem>
       ))}
-    </ul>
+    </List>
   ) : (
     <p>Loading</p>
   );
