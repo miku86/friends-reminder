ea0fcfee934557ecb0b871d3ab94807219e08f78
miku86 <miku86coding@gmail.com>
2020-05-20 20:13:13 +0200

    Add small styling

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,16 +1,28 @@
+import { makeStyles, Theme } from "@material-ui/core";
 import React from "react";
 import FriendsList from "./FriendsList/FriendsList";
+
+const useStyles = makeStyles((theme: Theme) => ({
+  root: {
+    minHeight: "100vh",
+    backgroundColor: "hsl(0, 0%, 95%)",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center"
+  },
+}));
 
 interface Props {}
 
 export const App = (props: Props) => {
+  const classes = useStyles();
+
   return (
-    <div className="App">
+    <div className={classes.root}>
       <FriendsList />
     </div>
   );
 };
-
-
 
 export default App;
