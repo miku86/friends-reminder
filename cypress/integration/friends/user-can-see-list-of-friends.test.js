caa507ea8d0124540fc2aa3bc852caf26c6263a8
miku86 <miku86coding@gmail.com>
2020-05-21 13:41:49 +0200

    Prettier

--- cypress/integration/friends/user-can-see-list-of-friends.test.js
+++ cypress/integration/friends/user-can-see-list-of-friends.test.js
@@ -2,31 +2,28 @@ import { apiConfig } from "../../../src/config/api";
 
 const mockResponse = [
   {
-    friendId: "1",
     friendName: "Max",
-    lastContactTime: 1589977129
+    lastContactTime: 1589977129,
   },
   {
-    friendId: "2",
     friendName: "Moritz",
-    lastContactTime: 1589977130
-  }
-]
+    lastContactTime: 1589977130,
+  },
+];
 
-describe('friends', () => {
-  it('should show a list of all friends', () => {
-    cy.server({force404: true});
+describe("friends", () => {
+  it("should show a list of all friends", () => {
+    cy.server({ force404: true });
     cy.route({
       method: "GET",
       url: `*${apiConfig.routes.friends}`,
-      response: mockResponse
-    })
+      response: mockResponse,
+    });
 
     cy.visit("/");
 
-    mockResponse.forEach(({friendName}) => {
-      cy.contains(friendName)
-    })
-  })
-
-})
+    mockResponse.forEach(({ friendName }) => {
+      cy.contains(friendName);
+    });
+  });
+});
