d074c8aa829121239ad240a7a3d7b1dee152efe9
miku86 <miku86coding@gmail.com>
2020-05-22 11:18:33 +0200

    Add first test for navbar

---
+++ src/components/Navbar/Navbar.test.js
@@ -0,0 +1,20 @@
+import { render } from "@testing-library/react";
+import React from "react";
+import { Provider } from "react-redux";
+import store from "../../state/store";
+import { Navbar } from "./Navbar";
+
+describe("Navbar", () => {
+  it("should only show signup and signin if not authenticated", () => {
+    const context = render(
+      <Provider store={store}>
+        <Navbar isAuthenticated={false} />
+      </Provider>
+    );
+    const { getByLabelText, queryByLabelText } = context;
+
+    expect(getByLabelText("signup-button")).toBeInTheDocument();
+    expect(getByLabelText("signin-button")).toBeInTheDocument();
+    expect(queryByLabelText("signout-button")).not.toBeInTheDocument();
+  });
+});
