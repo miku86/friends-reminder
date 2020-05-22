37a5a369ae5562f3daa78d14410e214d12352ac9
miku86 <miku86coding@gmail.com>
2020-05-22 16:10:35 +0200

    Add test for signin error

--- src/components/Signin/Signin.test.js
+++ src/components/Signin/Signin.test.js
@@ -1,38 +1,57 @@
 import { render } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import React from "react";
-import { createMockCredentials } from "../../utils/mockData";
+import {
+  createMockCredentials,
+  createWrongCredentials,
+} from "../../utils/mockData";
 import { Signin } from "./Signin";
 
 describe("Signin", () => {
-  let context;
   let signin;
 
   beforeEach(() => {
     signin = jest.fn().mockName("signin");
-    context = render(<Signin signin={signin} />);
   });
 
   it("should show a button to signin", () => {
-    const { getByLabelText } = context;
+    const { getByLabelText } = render(<Signin />);
+
     expect(getByLabelText("signin-button")).toBeInTheDocument();
   });
 
   it("should open a modal when clicking the signin button", async () => {
-    const { getByLabelText } = context;
+    const { getByLabelText } = render(<Signin />);
     await userEvent.click(getByLabelText("signin-button"));
+
     expect(getByLabelText("signin-modal")).toBeInTheDocument();
   });
 
   it("should run the signin function", async () => {
     const credentials = createMockCredentials();
     const { email, password } = credentials;
-    const { getByLabelText } = context;
+    const { getByLabelText } = render(<Signin signin={signin} />);
 
     await userEvent.click(getByLabelText("signin-button"));
     userEvent.type(getByLabelText("E-Mail"), email);
     userEvent.type(getByLabelText("Password"), password);
     await userEvent.click(getByLabelText("signin-submit"));
+
     expect(signin).toHaveBeenCalledWith({ email, password });
+  });
+
+  it("should show an error when signin failed", async () => {
+    const wrongCredentials = createWrongCredentials();
+    const { email, password } = wrongCredentials;
+    const { getByLabelText } = render(
+      <Signin signin={signin} authError={"error"} />
+    );
+
+    await userEvent.click(getByLabelText("signin-button"));
+    userEvent.type(getByLabelText("E-Mail"), email);
+    userEvent.type(getByLabelText("Password"), password);
+    await userEvent.click(getByLabelText("signin-submit"));
+
+    expect(getByLabelText("signin-error")).toBeInTheDocument();
   });
 });
