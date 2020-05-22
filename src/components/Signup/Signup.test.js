92825ccec8a9f272e8eecb8ba9320fc97181b178
miku86 <miku86coding@gmail.com>
2020-05-22 16:24:24 +0200

    Add test for signup error

--- src/components/Signup/Signup.test.js
+++ src/components/Signup/Signup.test.js
@@ -5,21 +5,19 @@ import { createMockCredentials } from "../../utils/mockData";
 import { Signup } from "./Signup";
 
 describe("Signup", () => {
-  let context;
   let signup;
 
   beforeEach(() => {
     signup = jest.fn().mockName("signup");
-    context = render(<Signup signup={signup} />);
   });
 
   it("should show a button to signup", () => {
-    const { getByLabelText } = context;
+    const { getByLabelText } = render(<Signup />);
     expect(getByLabelText("signup-button")).toBeInTheDocument();
   });
 
   it("should open a modal when clicking the signup button", async () => {
-    const { getByLabelText } = context;
+    const { getByLabelText } = render(<Signup signup={signup} />);
     await userEvent.click(getByLabelText("signup-button"));
     expect(getByLabelText("signup-modal")).toBeInTheDocument();
   });
@@ -27,12 +25,20 @@ describe("Signup", () => {
   it("should run the signup function", async () => {
     const credentials = createMockCredentials();
     const { email, password } = credentials;
-    const { getByLabelText } = context;
+    const { getByLabelText } = render(<Signup signup={signup} />);
 
     await userEvent.click(getByLabelText("signup-button"));
     userEvent.type(getByLabelText("E-Mail"), email);
     userEvent.type(getByLabelText("Password"), password);
     await userEvent.click(getByLabelText("signup-submit"));
     expect(signup).toHaveBeenCalledWith({ email, password });
+  });
+
+  it("should show an error when signup failed", async () => {
+    const { getByLabelText } = render(<Signup authError={"error"} />);
+
+    await userEvent.click(getByLabelText("signup-button"));
+
+    expect(getByLabelText("signup-error")).toBeInTheDocument();
   });
 });
