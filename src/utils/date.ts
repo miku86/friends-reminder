eb0a60e14750c052a4f5c9c3a28e96a6478a9d34
miku86 <miku86coding@gmail.com>
2020-05-24 10:17:08 +0200

    Add function to convert human time to timestamp

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -1,3 +1,8 @@
 export const convertTimestampToHumanTime = (timestamp: number): string => {
-  return (new Date(timestamp * 1000)).toLocaleDateString();
-}
+  return new Date(timestamp * 1000).toLocaleDateString();
+};
+
+export const convertHumanTimeToTimeStamp = (humanTime: Date | null): number => {
+  if (!humanTime) return 0;
+  return humanTime.valueOf();
+};
