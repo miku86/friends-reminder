1e0f0a942fc64399af7dbe84133837d113a674b8
miku86 <miku86coding@gmail.com>
2020-05-25 16:30:47 +0200

    Move signin dialog into separate component

--- src/components/Signin/Signin.tsx
+++ src/components/Signin/Signin.tsx
@@ -1,26 +1,11 @@
-import {
-  Button,
-  Dialog,
-  DialogActions,
-  DialogContent,
-  DialogContentText,
-  DialogTitle,
-  TextField,
-} from "@material-ui/core";
-import { Alert } from "@material-ui/lab";
-import React, { ChangeEvent, FormEvent, useState } from "react";
-import { connect } from "react-redux";
-import { signin } from "../../state/authSlice";
-import { AppState, AuthState, Credentials } from "../../utils/types";
+import { Button } from "@material-ui/core";
+import React, { useState } from "react";
+import SigninDialog from "../SigninDialog/SigninDialog";
 
-interface Props {
-  authError: AuthState["authError"];
-  signin?: (credentials: Credentials) => void;
-}
+interface Props {}
 
-export const Signin = ({ authError, signin }: Props) => {
+export const Signin = (props: Props) => {
   const [open, setOpen] = useState(false);
-  const [credentials, setCredentials] = useState({ email: "", password: "" });
 
   const handleClickOpen = () => {
     setOpen(true);
@@ -28,19 +13,6 @@ export const Signin = ({ authError, signin }: Props) => {
 
   const handleClose = () => {
     setOpen(false);
-  };
-
-  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
-    const target = event.currentTarget;
-    setCredentials((prev) => ({
-      ...prev,
-      [target.id]: target.value,
-    }));
-  };
-
-  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    signin!({ email: credentials.email, password: credentials.password });
   };
 
   return (
@@ -52,54 +24,9 @@ export const Signin = ({ authError, signin }: Props) => {
       >
         Signin
       </Button>
-      <Dialog open={open} onClose={handleClose} aria-label="signin-modal">
-        <form onSubmit={handleSubmit}>
-          <DialogTitle>Signin</DialogTitle>
-          <DialogContent>
-            {authError && (
-              <Alert severity="error" aria-label="signin-error">
-                {authError}
-              </Alert>
-            )}
-            <DialogContentText>
-              Demo: demo@miku86.com / demo123
-            </DialogContentText>
-            <TextField
-              autoFocus
-              margin="dense"
-              id="email"
-              label="E-Mail"
-              type="text"
-              fullWidth
-              onChange={handleChange}
-            />
-            <TextField
-              autoFocus
-              margin="dense"
-              id="password"
-              label="Password"
-              type="password"
-              fullWidth
-              onChange={handleChange}
-            />
-          </DialogContent>
-          <DialogActions>
-            <Button color="primary" aria-label="signin-submit" type="submit">
-              Signin
-            </Button>
-          </DialogActions>
-        </form>
-      </Dialog>
+      <SigninDialog open={open} handleClose={handleClose} />
     </>
   );
 };
 
-const mapStateToProps = (state: AppState) => ({
-  authError: state.auth.authError,
-});
-
-const mapDispatchToProps = {
-  signin,
-};
-
-export default connect(mapStateToProps, mapDispatchToProps)(Signin);
+export default Signin;
